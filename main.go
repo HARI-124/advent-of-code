53f4d1af63e4402f10e6b4c9a468b63c2a7c27a6
Harisudarsan <sudarshanhari561@gmail.com>
2023-07-31 22:08:03 +0530

    day 6 part 1

--- main.go
+++ main.go
@@ -7,33 +7,33 @@ import (
 	"strings"
 )
 
-func Intersection(a []string) (c string) {
+func FindUnique(a, b, c, d byte) bool {
+	occurences := make(map[byte]bool)
+	occurences[a] = true
+	occurences[b] = true
+	occurences[c] = true
+	occurences[d] = true
 
-	set1 := make(map[string]bool)
-	set2 := make(map[string]bool)
-	set3 := make(map[string]bool)
-
-	for _, item := range a[0] {
-
-		set1[string(item)] = true
-	}
-	for _, item := range a[1] {
-
-		set2[string(item)] = true
-	}
-	for _, item := range a[2] {
-
-		set3[string(item)] = true
+	if len(occurences) == 4 {
+		return true
 	}
 
-	for char := range set1 {
+	return false
 
-		if set2[char] && set3[char] {
-			c = char
+}
+
+func Iterate(a string) {
+
+	for i := 0; i < len(a); i++ {
+
+		// end := i+14
+		result := FindUnique(a[i], a[i+1], a[i+2], a[i+3])
+		if result {
+			fmt.Println(i + 3)
+			return
 		}
 	}
 
-	return
 }
 
 func main() {
@@ -47,38 +47,7 @@ func main() {
 	content, err := ioutil.ReadAll(file)
 
 	lines := strings.Split(string(content), "\n")
+	// fmt.Println("sample", len(lines))
 
-	paragraphs := make([]string, 0)
-	for i := 0; i < len(lines); i += 3 {
-		end := i + 3
-		if end > len(lines) {
-			end = len(lines)
-		}
-		paragraph := strings.Join(lines[i:end], "\n")
-		paragraphs = append(paragraphs, paragraph)
-	}
-	score := 0
-
-	for _, paragraph := range paragraphs {
-
-		rack := strings.Split(paragraph, "\n")
-		if len(rack) > 2 {
-
-			common := Intersection(rack)
-			fmt.Println(common)
-			ascii := []rune(common)
-			if int(ascii[0]) > 96 {
-				// fmt.Println(int(ascii[0]) - 96)
-				score = score + int(ascii[0]) - 96
-			} else {
-
-				// fmt.Println(int(ascii[0]) - 38)
-				score = score + int(ascii[0]) - 38
-			}
-		}
-
-	}
-
-	fmt.Println(score)
-
+	Iterate(lines[0])
 }
